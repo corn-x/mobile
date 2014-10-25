56f303b29e2c26236b952fe6ddc618cad001ffac
baggus <alekciepiela@gmail.com>
2014-10-25 08:22:23 +0200

    teamService

--- app/src/main/java/cornx/meetly/app/AppModule.java
+++ app/src/main/java/cornx/meetly/app/AppModule.java
@@ -11,9 +11,12 @@ import cornx.meetly.team.MemberProviderDummy;
 import cornx.meetly.team.TeamFragment;
 import cornx.meetly.team.TeamProvider;
 import cornx.meetly.team.TeamProviderDummy;
+import cornx.meetly.team.TeamProviderImpl;
+import cornx.meetly.team.TeamService;
 import cornx.meetly.team.TeamsFragment;
 import dagger.Module;
 import dagger.Provides;
+import retrofit.RestAdapter;
 
 /**
  * @author Aleksander Ciepiela
@@ -22,16 +25,19 @@ import dagger.Provides;
 @Module(
         library = true,
         injects = {MeetlyApplication.class, TeamsFragment.class, TeamProviderDummy.class,
-                TeamFragment.class}
+                TeamFragment.class, TeamProviderImpl.class}
 )
 
 
 public class AppModule {
 
+    private static final String SERVER = "http://";
     private MeetlyApplication meetlyApplication;
+    private RestAdapter restAdapter;
 
     public AppModule(MeetlyApplication meetlyApplication) {
         this.meetlyApplication = meetlyApplication;
+        restAdapter = new RestAdapter.Builder().setEndpoint(SERVER).build();
     }
 
     @Provides
@@ -47,13 +53,20 @@ public class AppModule {
 
     @Provides
     @Singleton
-    public TeamProvider provideTeamProvider(Bus bus) {
-        return new TeamProviderDummy(bus);
+    public TeamProvider provideTeamProvider(TeamService teamService, Bus bus) {
+        /*return new TeamProviderDummy(bus);*/
+        return new TeamProviderImpl(teamService, bus);
     }
 
     @Provides
     @Singleton
     public MemberProvider provideMemberProvider(Bus bus) {
         return new MemberProviderDummy(bus);
+    }
+
+    @Provides
+    @Singleton
+    public TeamService provideTeamService() {
+        return restAdapter.create(TeamService.class);
     }
 }
